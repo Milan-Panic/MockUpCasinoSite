292501716e6f1dcb39612f8d8a6f8c64baeae793
[Milan-Panic] <[milanpanic88@gmail.com]>
2020-05-04 00:45:40 +0200

    Ispravljen responsiv

--- js/main.js
+++ js/main.js
@@ -86,3 +86,63 @@ btnScrollToTop.addEventListener("click", function () {
         behavior: "smooth"
     });
 })
+
+//slider za bonuses
+
+var trenutni = 1; 
+var drugi =  $('.slider-frame').length;
+var move = $('.prikaz-slajdova');
+move.html(` ${trenutni} / ${drugi} `);  
+
+$(document).ready(function(){
+    $('.next').on('click', function(){
+        var currentBloc = $('.current');
+        var nextBlock = currentBloc.next();
+        
+        if (trenutni < drugi) {
+            trenutni++;
+            move.html(` ${trenutni} / ${drugi} `);           
+        }
+
+
+        if (nextBlock.length) {
+            currentBloc.removeClass('current').css('z-index', -10);
+            nextBlock.addClass('current').css('z-index', 10);
+        }
+    });
+
+    $('.prev').on('click', function(){
+        var currentBloc = $('.current');
+        var prevBlock = currentBloc.prev();
+        
+        if (trenutni > 1) {
+            trenutni--;
+            move.html(` ${trenutni} / ${drugi} `);           
+        }
+
+
+        if (prevBlock.length == 0){
+            currentBloc.removeClass('current');
+            $('.slider-frame').eq(0).addClass('current')
+        }else{
+            currentBloc.removeClass('current').css('z-index', -10);
+            prevBlock.addClass('current').css('z-index', 10);
+        }
+    });
+});
+
+//menu button
+var clicked = true;
+$(document).ready(function(){
+    $('.show-menu').on('click', function(){
+        if (!clicked) {
+            $('#meny-links').css('display', 'none');
+            clicked = !clicked;
+            $('.fa-bars').removeClass('rotateMenu')
+        }else{
+            $('#meny-links').css('display', 'block');            
+            clicked = !clicked;
+            $('.fa-bars').addClass('rotateMenu')
+        }
+    })
+});
